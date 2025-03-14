907af17f8cc73edb86f4d10bcc471077e0163813
Martin-SK-PD <119045574+Martin-SK-PD@users.noreply.github.com>
2025-03-14 22:42:13 +0100

    add navigation bar and footer

--- my-app-fe/src/App.jsx
+++ my-app-fe/src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
 
-function App() {
- 
+import Navigation from './components/navigation';
+import Footer from "./components/footer"; 
+import { BrowserRouter } from 'react-router-dom'
 
+function App() {
+  
   return (
-    <>
-    </>
-  )
+    
+    <BrowserRouter>
+      <Navigation />
+      <main />
+      <Footer />
+    </BrowserRouter>
+          
+  );
 }
 
 export default App
+ 
---
+++ my-app-fe/src/components/footer.jsx
@@ -0,0 +1,15 @@
+
+
+function Footer(){
+
+    return (
+    <footer className="footer py-1 bg-light text-dark mt-auto ">
+        <div className="container-fluid text-center">
+             <span > &copy; 2025  - Košovský Martin UK FMFI.</span> 
+        </div>
+    </footer>
+    ); 
+     
+}
+
+export default Footer
