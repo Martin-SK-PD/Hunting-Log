c4a1efc885266ef2c29c51dad58d1e5c94a6953e
Martin-SK-PD <119045574+Martin-SK-PD@users.noreply.github.com>
2025-03-22 22:32:46 +0100

    add backend route for user registration

--- my-app-be/app.js
+++ my-app-be/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import logger from "morgan";
 
+import registerRoute from "./routes/register.js"
+
 
 const app = express();
 
@@ -9,6 +11,8 @@ app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 
+
+app.use("/register", registerRoute);
 
 app.get("/", (req, res) => {
     res.send("Server is running!");
---
+++ my-app-be/routes/register.js
@@ -0,0 +1,102 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import pool from "../db.js";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+ 
+  const {
+    username, email, password, first_name, last_name, 
+    street, house_number, postal_code, city, 
+    is_admin, hunting_ground_id, hunting_ground_name
+  } = req.body;
+
+  try {
+
+    if (
+      !username || !email || !password || !first_name || !last_name ||
+      !street || !house_number || !postal_code || !city ||
+      !hunting_ground_id ||
+      (is_admin && !hunting_ground_name)
+    ) {
+      return res.status(400).json({ error: "Missing required registration fields" });
+    }
+
+
+
+    // Overenie, či užívateľ s daným emailom alebo menom už existuje
+    const userExists = await pool.query(
+      "SELECT * FROM users WHERE email = $1 OR username = $2",
+      [email, username]
+    );
+    if (userExists.rows.length > 0) {
+      return res.status(401).json({ error: "Používateľ s týmto emailom alebo používateľským menom už existuje." });
+    }
+
+
+
+    const groundCheck = await pool.query(
+      "SELECT * FROM hunting_grounds WHERE id = $1",
+      [parseInt(hunting_ground_id)]
+    );
+
+    if (is_admin) {
+      // Ak je admin, revír nesmie existovať
+      if (groundCheck.rows.length > 0) {
+        return res.status(400).json({ error: "Pre tento revír už existuje spravca, správcu nemožno pridať." });
+      }
+
+      // Vytvorenie nového revíru
+      await pool.query(
+        "INSERT INTO hunting_grounds (id, name) VALUES ($1, $2)",
+        [parseInt(hunting_ground_id), hunting_ground_name]
+      );
+
+    } else {
+      // Ak je hunter, revír musí existovať
+      if (groundCheck.rows.length === 0) {
+        return res.status(400).json({ error: "Zvolený revír neexistuje." });
+      }
+    }
+
+
+    // Hashovanie hesla
+    const salt = await bcrypt.genSalt(10);
+    const bcryptPassword = await bcrypt.hash(password, salt);
+
+
+    // Vytvorenie používateľa
+    const newUser = await pool.query(
+      `INSERT INTO users (username, email, password_hash, first_name, last_name, street, house_number, postal_code, city)
+       VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING id`,
+      [
+        username, email, bcryptPassword,
+        first_name, last_name,
+        street, house_number, postal_code, city
+      ]
+    );
+
+    const userId = newUser.rows[0].id;
+
+    
+
+    // Priradenie používateľa do user_hunting_ground s príslušnou rolou
+    const role = is_admin ? "Admin" : "Hunter";
+    console.log("Vkladám do user_hunting_ground:", userId, hunting_ground_id, role);
+
+    await pool.query(
+    "INSERT INTO user_hunting_ground (user_id, hunting_ground_id, role) VALUES ($1, $2, $3)",
+    [userId, parseInt(hunting_ground_id), role]
+    );
+    
+    return res.status(200).json({ message: "Registration successful" });
+
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+export default router;
