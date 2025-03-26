d07e81c8e69d5c12b2360eac28b4413ab5871f32
Martin-SK-PD <119045574+Martin-SK-PD@users.noreply.github.com>
2025-03-26 15:19:29 +0100

    remove exposed .env and update secrets

--- my-app-be/db.js
+++ my-app-be/db.js
@@ -1,12 +1,14 @@
 import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "admin",
-  port: 5432,
-  database: "Hunting_log",
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  port: process.env.port,
+  database: process.env.database,
 });
 
 export default pool;
