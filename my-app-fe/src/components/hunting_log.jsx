e38b33b73ad17975e12fc9627c4b9e67a7809a5b
Martin-SK-PD <119045574+Martin-SK-PD@users.noreply.github.com>
2025-03-29 13:13:05 +0100

    update hunting log page to use hunting record table component

--- my-app-fe/src/components/hunting_log.jsx
+++ my-app-fe/src/components/hunting_log.jsx
@@ -1,58 +1,48 @@
-import Announcement_card from "./announcement";
+import { useEffect, useState } from "react";
+import Hunting_records_table from "./hunting_records_table"; 
 
+function Hunting_log() {
+  const [records, setRecords] = useState([]);
 
-function Hunting_log(){
-  
+  const fetchRecords = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/api/v1/hunting-records", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      if (!res.ok) throw new Error("Chyba pri načítaní úlovkov");
+      const data = await res.json();
+      setRecords(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecords();
+    const interval = setInterval(fetchRecords, 10000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-lg-4 order-lg-2">
-          <Announcement_card />
-        </div>
+        <div className="col-lg-12">
 
-        <div className="col-lg-8 order-lg-1">
+          <div className="card scrollable-card my-md-3 m-3 p-2">
+            <div className="card-title px-3 pt-2">
+              <h4 className="text-start font-weight-bold m-2">Záznamy o úlovkoch</h4>
+            </div>
 
-        <div className="card my-md-3 m-3 p-2 h-100">
-          <div className="card-title px-3 pt-2">
-            <h4 className="text-start font-weight-bold m-2">Záznamy zo dňa: 13.3.2025 </h4>
+            <div className="card-body">
+              <Hunting_records_table records={records} />
+            </div>
           </div>
-
-
-          <div className="card-body">
-                             
-          <table className="table table-striped table-hover text-center">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Lovec</th>
-                <th scope="col">Oblasť</th>
-                <th scope="col">Miesto/Štruktúra</th>
-                <th scope="col">Ulovená zver</th>
-                <th scope="col">Váha</th>
-                <th scope="col">Čas strelenia</th>    
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Tručka M.</td>
-                <td>Medziriečie</td>
-                <td>Posed pod lesom</td>
-                <td>Jeleň</td>
-                <td>200 kg</td>
-                <td>23:15</td>
-              </tr>
-                            
-            </tbody>
-          </table>
         </div>
       </div>
     </div>
-  </div>
-  </div>
   );
 }
 
-
-            
-export default Hunting_log
+export default Hunting_log;
